c93c25883d52bf166e35e2b6735059e7e9bfa498
Ubuntu <ubuntu@ip-172-31-6-202.ap-northeast-2.compute.internal>
2022-11-15 07:01:19 +0000

    priority_change clear

--- threads/thread.c
+++ threads/thread.c
@@ -230,7 +230,7 @@ thread_create (const char *name, int priority,
 
 	/* Add to run queue. */
 	thread_unblock (t);
-
+	thread_yield();
 	return tid;
 }
 
@@ -338,6 +338,7 @@ thread_yield (void) {
 void
 thread_set_priority (int new_priority) {
 	thread_current ()->priority = new_priority;
+	thread_yield();
 }
 
 /* Returns the current thread's priority. */
